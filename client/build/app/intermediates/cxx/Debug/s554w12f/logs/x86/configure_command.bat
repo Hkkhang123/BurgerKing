2658e727d455b798f8cee406b7e83ea98aadad7e
Hkkhang123 <kienkhanghoang123@gmail.com>
2025-05-19 09:01:50 +0700

    first commit

---
+++ client/build/app/intermediates/cxx/Debug/s554w12f/logs/x86/configure_command.bat
@@ -0,0 +1,20 @@
+@echo off
+"C:\\Users\\kienk\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\kienk\\AppData\\Local\\Flutter-SDK\\flutter\\packages\\flutter_tools\\gradle\\src\\main\\groovy" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=x86" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86" ^
+  "-DANDROID_NDK=C:\\Users\\kienk\\AppData\\Local\\Android\\sdk\\ndk\\26.3.11579264" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\kienk\\AppData\\Local\\Android\\sdk\\ndk\\26.3.11579264" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\kienk\\AppData\\Local\\Android\\sdk\\ndk\\26.3.11579264\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\kienk\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\kienk\\OneDrive\\Desktop\\Pizza\\client\\build\\app\\intermediates\\cxx\\Debug\\s554w12f\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\kienk\\OneDrive\\Desktop\\Pizza\\client\\build\\app\\intermediates\\cxx\\Debug\\s554w12f\\obj\\x86" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BC:\\Users\\kienk\\OneDrive\\Desktop\\Pizza\\client\\android\\app\\.cxx\\Debug\\s554w12f\\x86" ^
+  -GNinja ^
+  -Wno-dev ^
+  --no-warn-unused-cli
