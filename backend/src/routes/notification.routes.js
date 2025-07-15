a522b85c52756ab7d4975050cfbe44dfb016a146
Hkkhang123 <kienkhanghoang123@gmail.com>
2025-07-15 21:06:42 +0700

    fix nofitication 2

--- backend/src/routes/notification.routes.js
+++ backend/src/routes/notification.routes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { getNotifications, createNotification, markAllAsRead } from "../controllers/notification.controller.js";
-import { verifyToken } from "../middleware/auth.middleware.js";
+import { protectRoutes } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", verifyToken, getNotifications);
+router.get("/", protectRoutes, getNotifications);
 router.post("/", createNotification); // Có thể cần verifyToken nếu chỉ cho phép user đã đăng nhập
-router.post("/mark-all-read", verifyToken, markAllAsRead);
+router.post("/mark-all-read", protectRoutes, markAllAsRead);
 
 export default router; 
